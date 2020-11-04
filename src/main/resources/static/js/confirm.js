b95966707e0c91cb0b65a95140efd852eb47cd30
Zin Htett <zin.minhtett1997@gmail.com>
2020-11-04 23:56:30 +0630

    feat : change ui table style

--- src/main/resources/static/js/confirm.js
+++ src/main/resources/static/js/confirm.js
@@ -1,31 +1,41 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    $('.nBtn, .table .eBtn').on('click', function(event) {
-        event.preventDefault();
-        var href = $(this).attr('href');
-        var text = $(this).text();
-        //for update user
-        if (text == 'Edit') {
-            $.get(href, function (users, status) {
-                $('.myFormUpdate #id').val(users.id);
-                $('.myFormUpdate #username').val(users.username);
-                $('.myFormUpdate #password').val(users.password);
-                $('.myFormUpdate #email').val(users.email);
-            });
-            $('.myFormUpdate #updateModal').modal();
-        } else {
-            //for creating user
-            $('.myFormCreate #username').val('');
-            $('.myFormCreate #password').val('');
-            $('.myFormCreate #email').val('');
-            $('.myFormCreate #myModalCreate').modal();
-        }
-    });
-    //for delete user
-    $('.table .delBtn').on('click', function(event) {
-        event.preventDefault();
-        var href = $(this).attr('href');
-        $('#removeModalCenter #delRef').attr('href', href);
-        $('#removeModalCenter').modal();
-    });
+	$('.nBtn, .table .eBtn').on('click', function(event) {
+		event.preventDefault();
+		var href = $(this).attr('href');
+		var text = $(this).text();
+		//for update user
+		if (text == 'Edit') {
+			$.get(href, function(users, status) {
+				$('.myFormUpdate #id').val(users.id);
+				$('.myFormUpdate #username').val(users.username);
+				$('.myFormUpdate #password').val(users.password);
+				$('.myFormUpdate #email').val(users.email);
+			});
+			$('.myFormUpdate #updateModal').modal();
+		} else {
+			//for creating user
+			$('.myFormCreate #username').val('');
+			$('.myFormCreate #password').val('');
+			$('.myFormCreate #email').val('');
+			$('.myFormCreate #myModalCreate').modal();
+		}
+	});
+	//for delete user
+	$('.table .delBtn').on('click', function(event) {
+		event.preventDefault();
+		var href = $(this).attr('href');
+		$('#removeModalCenter #delRef').attr('href', href);
+		$('#removeModalCenter').modal();
+	});
+
+
+});
+//for table
+$(document).ready(function() {
+	$('#dtDynamicVerticalScrollExample').DataTable({
+		"scrollY": "50vh",
+		"scrollCollapse": true,
+	});
+	$('.dataTables_length').addClass('bs-select');
 });
